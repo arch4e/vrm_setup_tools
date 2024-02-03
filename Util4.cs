aff4c8a92565faff0412b0c9293dbb1523770cbf
arch4e <contact@arch4e.com>
2024-02-03 18:47:00 +0900

    refactor

--- Util4.cs
+++ Util4.cs
@@ -64,6 +64,7 @@ namespace Util4
                     SkinnedMeshRenderer[] renderers = avatarPrefab.GetComponentsInChildren<SkinnedMeshRenderer>();
                     foreach (var renderer in renderers) CreateBlendShapeClipsFromSMR(renderer);
                 } else if (sourceOptions[selectedSourceIndex] == "Mesh") CreateBlendShapeClipsFromSMR(skinnedMeshRenderer);
+                Debug.Log("Util4: The creation of blend shape clips has been completed.");
             }
 
             if (GUILayout.Button("Remove Null Clips")) blendShapeObject.Clips.RemoveAll(item => item == null);
@@ -88,52 +89,37 @@ namespace Util4
                 // "selectedExistClipOptionIndex == 0" means "Skip"
                 if (string.IsNullOrEmpty(savePath) || (selectedExistClipOptionIndex == 0 && File.Exists(dataPath))) continue;
 
+                // define blend shape binding
+                BlendShapeBinding blendShapeBinding = new BlendShapeBinding();
+                blendShapeBinding.RelativePath      = meshRelativePath;
+                blendShapeBinding.Index             = i;
+                blendShapeBinding.Weight            = 100;
+
+                // add blend shape binding to blend shape clip
+                BlendShapeBinding[] blendShapeBindings = { blendShapeBinding };
+
                 int clipIndex = blendShapeObject.Clips.FindIndex(0, blendShapeObject.Clips.Count, x => x.name == mesh.GetBlendShapeName(i));
-                if (clipIndex == -1) {
+                if (clipIndex == -1) { // if the blend shape clip does not exist
                     // create new blend shape clip
                     var clip = BlendShapeAvatar.CreateBlendShapeClip(dataPath.ToUnityRelativePath());
                     blendShapeObject.Clips.Add(clip);
                     EditorUtility.SetDirty(blendShapeObject);
-
-                    // create blend shape binding
-                    BlendShapeBinding blendShapeBinding = new BlendShapeBinding();
-                    blendShapeBinding.RelativePath      = meshRelativePath;
-                    blendShapeBinding.Index             = i;
-                    blendShapeBinding.Weight            = 100;
-
-                    // add blend shape binding to blend shape clip
-                    BlendShapeBinding[] blendShapeBindings = { blendShapeBinding };
                     clip.Values = blendShapeBindings;
-                } else {
+                } else { // if the blend shape clip exists
                     BlendShapeBinding[] blendShapeBindingValues = blendShapeObject.Clips[clipIndex].Values;
-                    int blendShapeBindingValueIndex = Array.FindIndex(blendShapeBindingValues, x => x.Index == i);
-                    if (blendShapeBindingValueIndex == -1) {
-                        // create blend shape binding
-                        BlendShapeBinding blendShapeBinding = new BlendShapeBinding();
-                        blendShapeBinding.RelativePath      = meshRelativePath;
-                        blendShapeBinding.Index             = i;
-                        blendShapeBinding.Weight            = 100;
-
-                        // add blend shape binding to blend shape clip
-                        BlendShapeBinding[] blendShapeBindings  = { blendShapeBinding };
+                    int blendShapeBindingValueIndex = Array.FindIndex(blendShapeBindingValues, x => x.RelativePath == meshRelativePath);
+                    Debug.Log(blendShapeBindingValueIndex);
+                    if (blendShapeBindingValues.Length == 0) {
                         blendShapeObject.Clips[clipIndex].Values = blendShapeBindings;
+                    } else if (blendShapeBindingValueIndex == -1) {
+                        // add the blend shape binding
+                        // when the blend shape clip is exists, but blend shape binding is not exists
+                        Array.Resize(ref blendShapeObject.Clips[clipIndex].Values, blendShapeObject.Clips[clipIndex].Values.Length + 1);
+                        blendShapeObject.Clips[clipIndex].Values[blendShapeObject.Clips[clipIndex].Values.Length - 1] = blendShapeBinding;
                     } else if (blendShapeObject.Clips[clipIndex].Values[blendShapeBindingValueIndex].Weight == 0) {
+                        // set the blend shape binding weight
+                        // when the blend shape clip and the blend shape binding exist, and the weight value is 0
                         blendShapeObject.Clips[clipIndex].Values[blendShapeBindingValueIndex].Weight = 100;
-                    } else {
-                        int _blendShapeBindingValueIndex = Array.FindIndex(blendShapeBindingValues, x => x.RelativePath == meshRelativePath);
-
-                        // create blend shape binding
-                        BlendShapeBinding blendShapeBinding = new BlendShapeBinding();
-                        blendShapeBinding.RelativePath      = meshRelativePath;
-                        blendShapeBinding.Index             = i;
-                        blendShapeBinding.Weight            = 100;
-
-                        // add blend shape binding to blend shape clip
-                        if (_blendShapeBindingValueIndex == -1) { // BlendShapeClip is exists, BlendShapeBinding is not exist
-                            BlendShapeBinding[] blendShapeBindings = { blendShapeBinding };
-                            Array.Resize(ref blendShapeObject.Clips[clipIndex].Values, blendShapeObject.Clips[clipIndex].Values.Length + 1);
-                            blendShapeObject.Clips[clipIndex].Values[blendShapeObject.Clips[clipIndex].Values.Length - 1] = blendShapeBinding;
-                        }
                     }
                 }
             }
